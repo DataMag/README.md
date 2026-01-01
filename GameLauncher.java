245a90a02d8ea32a535b9a502eefcf66d0cf1ccd
DataMag <maga95454545@gmail.com>
2026-01-01 16:52:59 +0300

    GameLauncher.java

--- GameLauncher.java
+++ GameLauncher.java
@@ -3,16 +3,16 @@ import java.awt.*;
 
 public class GameLauncher {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("мой игровой центр");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Тут выход разрешен, это главное меню
+        JFrame frame = new JFrame("Game Launcher");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setSize(500, 600);
-        frame.setLayout(new GridLayout(3, 1, 10, 10)); // 3 кнопки друг под другом
+        frame.setLayout(new GridLayout(3, 1, 10, 10)); 
 
-        JButton btn1 = new JButton("Крестики-нолики");
-        JButton btn2 = new JButton("Виселица");
+        JButton btn1 = new JButton("Tic-tac-joe");
+        JButton btn2 = new JButton("AdamAsmaca");
         JButton btn3 = new JButton("Flappy Bird");
 
-        // Привязываем запуск игр к кнопкам
+        // РџСЂРёРІСЏР·С‹РІР°РµРј Р·Р°РїСѓСЃРє РёРіСЂ Рє РєРЅРѕРїРєР°Рј
         btn1.addActionListener(e -> ste.main(null));
         btn2.addActionListener(e -> AdamAsmacaGUI.main(null));
         btn3.addActionListener(e -> FlappyBird.main(null));
@@ -25,4 +25,5 @@ public class GameLauncher {
         frame.setVisible(true);
     }
 }
+
         
